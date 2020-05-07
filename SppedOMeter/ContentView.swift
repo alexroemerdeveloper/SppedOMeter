89e36183e5515f54dc4e023a92a6d974dde18369
Alexander Römer <alex@roemer.design>
2020-05-07 13:23:56 +0200

    initial commit

--- SppedOMeter/ContentView.swift
+++ SppedOMeter/ContentView.swift
@@ -9,8 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors = [Color("Color"),Color("Color1")]
+    @State var progress : CGFloat = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Meter(progress: self.$progress)
+            HStack(spacing: 25) {
+                Button(action: {
+                    withAnimation(Animation.default.speed(0.55)) {
+                        self.progress += 10
+                    }
+                }) {
+                    Text("Update")
+                        .padding(.vertical,10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                }
+                .background(Capsule().stroke(LinearGradient(gradient: .init(colors: self.colors), startPoint: .leading, endPoint: .trailing), lineWidth: 2))
+                
+                Button(action: {
+                    withAnimation(Animation.default.speed(0.55)) {
+                        self.progress = 0
+                    }
+                }) {
+                    Text("Reset")
+                        .padding(.vertical,10)
+                        .frame(width: (UIScreen.main.bounds.width - 50) / 2)
+                }
+                .background(Capsule().stroke(LinearGradient(gradient: .init(colors: self.colors), startPoint: .leading, endPoint: .trailing), lineWidth: 2))
+            }
+            .padding(.top, 55)
+        }
     }
 }
 
